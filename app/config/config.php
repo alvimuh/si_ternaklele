2271a8dc9233821df5a313a00660c0cbee4d0f22
mohamadalvir <mohamadalvin@gmail.com>
2020-01-22 13:53:28 +0700

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+
+define('BASEURL', 'http://localhost/10517094/phpmvc_flasher/public');
+
+//db'
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','phpmvc');
---
+++ app/views/about/page.php
@@ -0,0 +1,5 @@
+
+<body>
+	 <h1>My Page</h1>
+	
+
